cf3920634ea82a331ad8281d99f564c8288c5432
Ali Can Yücel <yucelalican@hotmail.com>
2026-02-21 00:30:03 +0300

    Add project files.

---
+++ Yuksi/Yuksi.Application/Mapping/MappingProfile.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+
+namespace Yuksi.Application.Mapping
+{
+    public sealed class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+
+        }
+    }
+}
---
+++ Yuksi/Yuksi.Application/Services/IJwtProvider.cs
@@ -0,0 +1,10 @@
+﻿using Yuksi.Application.Features.Auth.Login;
+using Yuksi.Domain.Entities;
+
+namespace Yuksi.Application.Services
+{
+    public interface IJwtProvider
+    {
+        Task<LoginCommandResponse> CreateToken(AppUser user);
+    }
+}

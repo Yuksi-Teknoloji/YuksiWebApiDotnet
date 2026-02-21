a747d39d7609369fabaa76a4f789d8535e5e635b
EmreCanTERKAN <emrecanterkan06@gmail.com>
2026-02-22 00:15:02 +0300

    adres domaini tasarlandı feature/domain-design branc pr denemesi yapılıyor

--- Yuksi/Yuksi.Domain/Entities/Neon/Address.cs
+++ Yuksi/Yuksi.Domain/Entities/Neon/Address.cs
@@ -1,50 +1,28 @@
 ﻿namespace Yuksi.Domain;
 
-public partial class Address
+public sealed class Address
 {
     public Guid Id { get; set; }
-
     public string EntityType { get; set; } = null!;
-
     public Guid EntityId { get; set; }
-
     public string AddressType { get; set; } = null!;
-
     public string City { get; set; } = null!;
-
     public string District { get; set; } = null!;
-
     public string? Neighborhood { get; set; }
-
     public string? Street { get; set; }
-
     public string? BuildingNumber { get; set; }
-
     public string? ApartmentNumber { get; set; }
-
     public string? Floor { get; set; }
-
     public string? PostalCode { get; set; }
-
     public string? FullAddress { get; set; }
-
     public string? AddressTitle { get; set; }
-
     public string? ContactName { get; set; }
-
     public string? ContactPhone { get; set; }
-
     public decimal? Latitude { get; set; }
-
     public decimal? Longitude { get; set; }
-
     public bool? IsActive { get; set; }
-
     public bool? IsDefault { get; set; }
-
     public bool? Deleted { get; set; }
-
     public DateTime? CreatedAt { get; set; }
-
     public DateTime? UpdatedAt { get; set; }
 }
